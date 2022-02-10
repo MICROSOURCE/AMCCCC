b077d5816aa9cc729c60641399ce1bf50ead4e88
ikarttest <ikarttest@gmail.com>
2022-02-10 09:43:31 +0530

    Entity

---
+++ Entity/Login_Ent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Login_Ent
+    {
+        public string USER_ID { get; set; }
+        public string USER_NAME { get; set; }
+        public string USER_SRT_NAME { get; set; }
+        public string USER_PWD { get; set; }
+        public string ROLE_ID { get; set; }
+        public string EMP_ID { get; set; }
+
+        public string MOD_ID { get; set; }
+        public string MOD_ENAME { get; set; }
+        public string ROLE_NAME { get; set; }
+        public string MOD_PATH { get; set; }
+        public string MOD_DESC { get; set; }
+        public string MOD_CLASS { get; set; }
+    }
+}
---
+++ Entity/Rights_Entities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Rights_Entities
+    {
+        public string ROLE_ID { get; set; }
+        public string FORM_ID { get; set; }
+        public string R_ADD { get; set; }
+        public string R_EDIT { get; set; }
+        public string R_DEL { get; set; }
+        public string R_READ { get; set; }
+        public string S_EDIT { get; set; }
+        public string S_DEL { get; set; }
+        public string ACCESS { get; set; }
+    }
+}
